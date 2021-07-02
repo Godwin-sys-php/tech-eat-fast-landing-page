861341d42ad9b6b4343529eff849e979809fa9e3
Godwin Burume <godwinnyembo@gmail.com>
2021-07-02 12:15:43 +0100

    test server

--- app.js
+++ app.js
@@ -9,11 +9,18 @@ app.use(cors()); // On accepte toute les requêtes de n'importe quelle serveur
 
 app.use(helmet());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "html", "index.html"));
 });
 
-app.get(
+app.use(
   "/assets",
   express.static(path.join(__dirname, "html", "assets"))
 );
