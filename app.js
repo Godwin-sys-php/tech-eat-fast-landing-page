a5461906e05e32c36f21fa408d6faad75d52f9cd
Godwin Burume <godwinnyembo@gmail.com>
2021-07-02 12:11:15 +0100

    Ajout du serveur node

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "html", "index.html"));
 });
 
-app.use(
+app.get(
   "/assets",
   express.static(path.join(__dirname, "html", "assets"))
 );
